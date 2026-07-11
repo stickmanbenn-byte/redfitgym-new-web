3d8a615bbdd766fd0f54779807e620990ffc6165
stickmanbenn-byte <stickmanbenn@gmail.com>
2026-07-11 22:00:27 +0530

    Add new assets

--- src/assets/barbell/frames.ts
+++
@@ -1,6 +0,0 @@
-// Auto-generated barrel file for barbell frame sequence
-const modules = import.meta.glob<{ default: string }>('./*.jpg', { eager: true });
-
-export const BARBELL_FRAMES: string[] = Object.keys(modules)
-  .sort()
-  .map((key) => modules[key].default);
